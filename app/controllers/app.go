46ae3501acae028b114713bb3cf7eacfcdd218e4
taisuke <taisukeohmi0420@gmail.com>
2017-11-10 22:34:16 +0100

    first commit

---
+++ app/controllers/app.go
@@ -0,0 +1,27 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/revel/revel"
+)
+
+type App struct {
+	*revel.Controller
+}
+
+func (c App) Index() revel.Result {
+	g := "Aloha World"
+	return c.Render(g)
+}
+
+func (c App) Hello(myName string) revel.Result {
+	c.Validation.Required(myName).Message("Your name is required")
+	c.Validation.MinSize(myName, 3).Message("Your name is not long enough")
+
+	if c.Validation.HasErrors() {
+		c.Validation.Keep()
+		c.FlashParams()
+		return c.Redirect(App.Index)
+	}
+	return c.Render(myName)
+}
